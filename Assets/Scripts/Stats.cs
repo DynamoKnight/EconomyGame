20f739575bef8292cfa2245cd8bf9c71af69315a
lpher <lpher@uci.edu>
2026-05-06 15:59:43 -0700

    Reset starting points to zero

--- Assets/Scripts/Stats.cs
+++ Assets/Scripts/Stats.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using System.Collections.Generic;
 
 class Stats {
-    public static int points = 100;
+    public static int points = 0;
 
     public static List<SwordData> swordList { get; set; } = new List<SwordData>{}; 
 }
