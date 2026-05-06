834dd76f516518558477a6bd7b42221ec0a7ec0a
Nakul K <nakul.s.kumar@live.com>
2026-05-06 10:48:05 -0700

    Sigmoid function

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,6 +8,15 @@ public class GameManager : MonoBehaviour
 
     public bool isPaused = false;
 
+    void Awake(){
+
+        // SwordData(Name, Damage, Cost, Color)
+        Stats.swordList.Add(new SwordData("Wooden Sword",  CalculateDamage(0),    0,    Color.brown)); // 27 dmg
+        Stats.swordList.Add(new SwordData("Stone Sword",   CalculateDamage(200),  200,  Color.gray)); // 40 dmg
+        Stats.swordList.Add(new SwordData("Iron Sword",    CalculateDamage(500),  500,  Color.white)); // 58 dmg
+        Stats.swordList.Add(new SwordData("Gold Sword",    CalculateDamage(1000), 1000, Color.yellow)); // 65 dmg (THE TRAP)
+        Stats.swordList.Add(new SwordData("Diamond Sword", CalculateDamage(2200), 2200, Color.cyan)); // 98 dmg
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start(){
         shopMenu.SetActive(false);
@@ -39,5 +48,15 @@ public class GameManager : MonoBehaviour
             isPaused = true;
             Time.timeScale = 0;
         }
+    }
+
+    // Calculates damage based on cost. 
+    public int CalculateDamage(int cost){
+        // Uses a double sigmoid curve.
+        float baseDmg = 25f;
+        float w1 = 35f / (1.0f + Mathf.Exp(-0.01f * (cost - 250f)));  // First hump
+        float w2 = 40f / (1.0f + Mathf.Exp(-0.005f * (cost - 1600f))); // Second hump
+        
+        return Mathf.RoundToInt(baseDmg + w1 + w2);
     }
 }
